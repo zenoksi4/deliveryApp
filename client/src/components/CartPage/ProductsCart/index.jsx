e07f9ea9848dcef253032721ce81976d488eaae0
Pasha <zenoken4ik@gmail.com>
2023-05-30 22:19:44 +0300

    added store

--- client/src/components/CartPage/ProductsCart/index.jsx
+++ client/src/components/CartPage/ProductsCart/index.jsx
@@ -6,7 +6,7 @@ function ProductsCart() {
     return (
       <div className={styles.productsCartBox}>
         <div className={styles.productCard}>
-            <img className={styles.cardImg} src='https://www.resizepixel.com/Image/dkkarenjq/Preview/black_and_blue_burger_95881_16x9.jpg?v=0e4ab2da-4408-48e1-9446-a5fd923893bb'/>
+            <img className={styles.cardImg} alt='product' src='https://www.resizepixel.com/Image/dkkarenjq/Preview/black_and_blue_burger_95881_16x9.jpg?v=0e4ab2da-4408-48e1-9446-a5fd923893bb'/>
             <div className={styles.cardDesc}>
                 <h4>Big Big Burger</h4>
                 <p>price: 4$</p>
@@ -14,32 +14,6 @@ function ProductsCart() {
             </div>
         </div>
 
-        <div className={styles.productCard}>
-            <img className={styles.cardImg} src='https://www.resizepixel.com/Image/dkkarenjq/Preview/black_and_blue_burger_95881_16x9.jpg?v=0e4ab2da-4408-48e1-9446-a5fd923893bb'/>
-            <div className={styles.cardDesc}>
-                <h4>Big Big Burger</h4>
-                <p>price: 4$</p>
-                <Input type='number' className={styles.countCard}/>
-            </div>
-        </div>
-
-        <div className={styles.productCard}>
-            <img className={styles.cardImg} src='https://www.resizepixel.com/Image/dkkarenjq/Preview/black_and_blue_burger_95881_16x9.jpg?v=0e4ab2da-4408-48e1-9446-a5fd923893bb'/>
-            <div className={styles.cardDesc}>
-                <h4>Big Big Burger</h4>
-                <p>price: 4$</p>
-                <Input type='number' className={styles.countCard}/>
-            </div>
-        </div>
-
-        <div className={styles.productCard}>
-            <img className={styles.cardImg} src='https://www.resizepixel.com/Image/dkkarenjq/Preview/black_and_blue_burger_95881_16x9.jpg?v=0e4ab2da-4408-48e1-9446-a5fd923893bb'/>
-            <div className={styles.cardDesc}>
-                <h4>Big Big Burger</h4>
-                <p>price: 4$</p>
-                <Input type='number' className={styles.countCard}/>
-            </div>
-        </div>
         
       </div>
     );
--- client/src/components/ShopPage/ProductsShop/index.jsx
+++ client/src/components/ShopPage/ProductsShop/index.jsx
@@ -7,7 +7,7 @@ function ProductsShop() {
       <div className={styles.productShopWrapper}>
         <div className={styles.productCardContent}>
 
-            <img className={styles.cardImg} src='https://www.resizepixel.com/Image/dkkarenjq/Preview/black_and_blue_burger_95881_16x9.jpg?v=0e4ab2da-4408-48e1-9446-a5fd923893bb'/>
+            <img className={styles.cardImg} alt='product' src='https://www.resizepixel.com/Image/dkkarenjq/Preview/black_and_blue_burger_95881_16x9.jpg?v=0e4ab2da-4408-48e1-9446-a5fd923893bb'/>
             <div className={styles.cardDesc} >
                 <h3 className={styles.cardTitle} >Big Big Burger</h3>
                 <p className={styles.cardPrice}>22$</p>
@@ -15,34 +15,6 @@ function ProductsShop() {
             <Button>Add to Cart</Button>
         </div>
 
-        <div className={styles.productCardContent}>
-
-            <img className={styles.cardImg} src='https://www.resizepixel.com/Image/dkkarenjq/Preview/black_and_blue_burger_95881_16x9.jpg?v=0e4ab2da-4408-48e1-9446-a5fd923893bb'/>
-            <div className={styles.cardDesc} >
-                <h3 className={styles.cardTitle} >Big Big Burger</h3>
-                <p className={styles.cardPrice}>22$</p>
-            </div>
-            <Button>Add to Cart</Button>
-        </div>
-
-        <div className={styles.productCardContent}>
-
-            <img className={styles.cardImg} src='https://www.resizepixel.com/Image/dkkarenjq/Preview/black_and_blue_burger_95881_16x9.jpg?v=0e4ab2da-4408-48e1-9446-a5fd923893bb'/>
-            <div className={styles.cardDesc} >
-                <h3 className={styles.cardTitle} >Big Big Burger</h3>
-                <p className={styles.cardPrice}>22$</p>
-            </div>
-            <Button>Add to Cart</Button>
-        </div>
-        <div className={styles.productCardContent}>
-
-            <img className={styles.cardImg} src='https://www.resizepixel.com/Image/dkkarenjq/Preview/black_and_blue_burger_95881_16x9.jpg?v=0e4ab2da-4408-48e1-9446-a5fd923893bb'/>
-            <div className={styles.cardDesc} >
-                <h3 className={styles.cardTitle} >Big Big Burger</h3>
-                <p className={styles.cardPrice}>22$</p>
-            </div>
-            <Button>Add to Cart</Button>
-        </div>
 
 
 
